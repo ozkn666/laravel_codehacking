59c24f65a8f046116865e683d586305eb30773b0
ozkn <ozkn666@yahoo.co.jp>
2019-07-04 11:29:03 +0900

    users section working 100 percent -- except security

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+    protected $upload = '/images/';
     protected $fillable = ['file'];
+
+    public function getFileAttribute($photo){
+        return $this->upload . $photo;
+    }
 }
